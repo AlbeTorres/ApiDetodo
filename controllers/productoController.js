a57cdeef23592889751cf3472b09ae5c9b9778ec
albetorres <albertocorreoficial@gmail.com>
2022-08-22 14:37:44 -0400

    eso mismo

--- controllers/productoController.js
+++ controllers/productoController.js
@@ -34,7 +34,7 @@ exports.obtenerProductos= async(req, res)=>{
     try {
         const productos = await Producto.find({});
 
-        res.status(200).json(productos);
+        res.status(200).json({productos});
         
     } catch (error) {
         console.log(error)
